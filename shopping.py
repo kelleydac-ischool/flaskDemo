0fa79462436c152f976fa420bb1855016f160489
Douglas Kelley <doug@orca5.local>
2015-11-21 15:17:43 -0800

    added nav bar and bootstrap forms

--- shopping.py
+++ shopping.py
@@ -1,16 +1,35 @@
 #! /usr/bin/env python27
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask.ext.moment import Moment
+from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
 
+from datetime import datetime
+
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(Form):
+    name = StringField('What item would you like to add?', validators=[Required()])
+    submit = SubmitField('Submit')
+    
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'BigSeekritNoboddyNoze'
+moment = Moment(app)
+bootstrap = Bootstrap(app)
 
 shoppingList = ['Milk', 'Eggs']
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global shoppingList
-    if request.method == 'POST':
-        shoppingList.append(request.form['item'])
-    return render_template("index.html", items = shoppingList)
+    item = None
+    form = NameForm()
+    if form.validate_on_submit():
+    	shoppingList.append(form.name.data)
+    	form.name.data=''
+    return render_template("index.html", items = shoppingList, current_time=datetime.utcnow(), form = form)
 
 @app.route('/remove/<name>')
 def remove_item(name):
@@ -26,6 +45,14 @@ def remove_item(name):
 def get_items():
 	global shoppingList
 	return jsonify({'items': shoppingList})
+	
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 	 
 if __name__ == '__main__':
     app.run(debug=True)
